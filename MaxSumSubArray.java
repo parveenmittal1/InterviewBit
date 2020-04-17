e6e845446d120b2f6fab609f8750bdeec08f2c4f
parveen <mittalparveen652@gmail.com>
2020-04-17 16:31:50 +0530

    MaxSumSubArray

--- MaxSumSubArray.java
+++ MaxSumSubArray.java
@@ -2,61 +2,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MaxSumSubArray {
-//    public static int maxSubArray(final List<Integer> A) {
-//        if(A.size()==0){
-//            return 0;
-//        }
-//        if(A.size()==1){
-//            return A.get(0);
-//        }
-//
-//        int maxSum=A.get(0);
-//        ArrayList<Long> sum=new ArrayList<>();
-//
-//        for(int i=0;i<A.size();i++){
-//            long tempSum=0;
-//      if (A.get(i) > 0) {
-//        while (A.get(i) > 0) {
-//          tempSum = tempSum + A.get(i);
-//          i++;
-//        }
-//      } else {
-//        while (A.get(i) < 0) {
-//          tempSum = tempSum + A.get(i);
-//          i++;
-//        }
-//        }
-//      sum.add(tempSum);
-//        }
-//
-//        long max =0;
-//        int start=0;
-//        int next=1;
-//        long temp=0;
-//        if(sum.get(start)>0)
-//        temp=sum.get(start);
-//        else {
-//            start++;
-//            next++;
-//            temp=sum.get(start);
-//        }
-//        while(next<sum.size()-1){
-//            if(sum.get(next)+sum.get(next+1)>0){
-//                temp=temp+sum.get(next)+sum.get(next+1);
-//                next=next+2;
-//            }
-//            else if(sum.get(next)>0) {
-//                temp=temp+sum.get(next);
-//            }
-//            if(max<temp){
-//                max=temp;
-//            }
-//
-//
-//
-//        }
-//    }
-        // DO NOT MODIFY THE LIST. IT IS READ ONLY
         public static int  maxSubArray(final List<Integer> A) {
             if(A.size()==0){
                 return 0;
