d12e0ee859ccb2e78eba4192e5410ada36f78dfa
parveen <mittalparveen652@gmail.com>
2020-04-21 19:41:33 +0530

    setmatrix

--- SetMatrixZeros.java
+++ SetMatrixZeros.java
@@ -3,45 +3,60 @@ import java.util.ArrayList;
 public class SetMatrixZeros {
 
     public static void setZeroes(ArrayList<ArrayList<Integer>> a) {
-        ArrayList<ArrayList<Boolean>> b=new ArrayList<>();
-
         for(int i=0;i<a.size();i++){
-            b.add(new ArrayList<>());
             for(int j=0;j<a.get(0).size();j++){
-                b.get(i).add(a.get(i).get(j)==0);
+                if(a.get(i).get(j)==0){
+                    a.get(i).set(0,0);
+                    a.get(0).set(j,0);
+                };
+        System.out.print(" "+a.get(i).get(j));
             }
+      System.out.println();
         }
 
-        for(int i=0;i<a.size();i++){
-            for(int j=0;j<a.get(0).size();j++){
-                if(b.get(i).get(j)){
-                    for(int k=0;k<a.get(i).size();k++){
-                        a.get(i).set(k,0);
-                    }
+        for(int i=1;i<a.size();i++){
+            if(a.get(i).get(0)==0){
+                for(int j=0;j<a.get(i).size();j++){
+                    a.get(i).set(j,0);
+                }
+            }
+            }
+
+
+            for(int j=1;j<a.get(0).size();j++){
+                if(a.get(0).get(j)==0){
                     for(int k=0;k<a.size();k++){
                         a.get(k).set(j,0);
                     }
                 }
+                }
+
+            if(a.get(0).get(0)==0){
+                for(int i=0;i<a.size();i++){
+                    a.get(i).set(0,0);
+                }
+
+                for(int k=0;k<a.get(0).size();k++){
+                    a.get(0).set(k,0);
+                }
             }
-        }
-
-
-    }
+            }
 
   public static void main(String[] args) {
         ArrayList<ArrayList<Integer>> temp=new ArrayList(3);
-    for (int i = 0; i < 2; i++) {
-        temp.add(new ArrayList<>());
-      for (int j = 0; j < 3; j++) {
-        temp.get(i).add(1);
-      }
-        }
-      temp.get(1).set(2,0);
-      setZeroes(temp);
-      for (int i = 0; i < 2; i++) {
-          for (int j = 0; j < 3; j++) {
-        System.out.println(temp.get(i).get(j));
+            for (int i = 0; i < 2; i++) {
+                temp.add(new ArrayList<>());
+                for (int j = 0; j < 3; j++) {
+                    temp.get(i).add(1);
+                }
+            }
+
+        temp.get(1).set(2,0);
+        setZeroes(temp);
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println(temp.get(i).get(j));
           }
-      }
+        }
+    }
   }
-}
--- SmallestMultipleWith0and1.java
+++ SmallestMultipleWith0and1.java
@@ -1,24 +1,38 @@
-import java.math.BigDecimal;
+
 import java.math.BigInteger;
 
 public class SmallestMultipleWith0and1 {
-    static String generatePrintBinary(int n){
-        return Integer.toBinaryString(n);
-    }
-    public static String multiple(int A) {
-        BigInteger temp2=BigInteger.valueOf(A);
-        int count=1;
-        BigInteger temp=new BigInteger((generatePrintBinary(count)));
-        BigInteger a=temp.remainder(temp2);
-        while(a.compareTo(new BigInteger(String.valueOf('0')))!=0){
-            temp=new BigInteger(generatePrintBinary(++count));
-            a=temp.remainder(temp2);
 
+    public static String generatePrintBinary(long n){
+        return Integer.toBinaryString((int)n);
+    }
+    static long numberZeroOne(int num){
+        String temp="";
+        int numTemp=num;
+        while(numTemp>10){
+            numTemp=numTemp/10;
+            temp='0'+temp;
+        }
+        temp='1'+temp;
+        return Integer.parseInt(temp,2);
+    }
+
+    public static String multiple(int A) {
+        long temp2 = (long)A;
+        long count = numberZeroOne(A);
+        long temp = Long.parseLong((generatePrintBinary(count)));
+        while (temp%temp2!=0) {
+            temp = Long.parseLong(generatePrintBinary(++count));
         }
         return String.valueOf(temp);
     }
-  public static void main(String[] args) {
-    System.out.println(
-    multiple(10000000));
-  }
+    public static void main(String[] args) {
+        long max=-1;
+        for(int i=1;i<2000;i++){
+            String temp=multiple(i);
+
+            System.out.println(
+                    i+" : "+generatePrintBinary(i)+" :"+multiple(i));
+        }
+    }
 }
